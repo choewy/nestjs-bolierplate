ecaa555d807bc903124a1a9398a643500310c09e
choewy <choewy32@gmail.com>
2024-02-05 19:46:43 +0900

    refactor/remove config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,6 @@ import 'dotenv/config';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { DataSourceConfig } from './config/datasource.config';
-
 import { MemberModule } from '@domain/member/member.module';
 import { FollowModule } from '@domain/follow/follow.module';
 
@@ -18,7 +16,20 @@ import { GetFollowingMembersUsecase } from './application/usecase/get-following-
 const AppUseCases = [InsertFollowMemberUsecase, GetFollowerMembersUsecase, GetFollowingMembersUsecase];
 
 @Module({
-  imports: [TypeOrmModule.forRoot(DataSourceConfig.moduleOptions), MemberModule, FollowModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATASOURCE_HOST,
+      port: +process.env.DATASOURCE_PORT,
+      username: process.env.DATASOURCE_USERNAME,
+      password: process.env.DATASOURCE_PASSWORD,
+      database: process.env.DATASOURCE_DATABASE,
+      entities: ['./dist/**/*/entity/*.entity.{js,ts}'],
+      synchronize: true,
+    }),
+    MemberModule,
+    FollowModule,
+  ],
   controllers: [HelloController, MemberController, FollowController],
   providers: AppUseCases,
 })
